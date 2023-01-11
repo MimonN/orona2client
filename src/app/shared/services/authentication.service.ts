dc0c2499a6f845696d3b988abb5b556230a67e58
MimonN <m.a.nedilsky@gmail.com>
2023-01-11 14:36:35 -0500

    Angular Authentication / Login Action

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthResponseDto } from 'src/app/interfaces/response/authResponseDto.model';
 import { RegistrationResponseDto } from 'src/app/interfaces/response/registrationResponseDto.model';
+import { UserForAuthenticationDto } from 'src/app/interfaces/user/userForAuthenticationDto.model';
 import { UserForRegistrationDto } from 'src/app/interfaces/user/userForRegistrationDto.model';
 import { environment } from 'src/environments/environment';
 
@@ -14,6 +16,10 @@ export class AuthenticationService {
 
   public registerUser = (route: string, body: UserForRegistrationDto) => {
     return this.http.post<RegistrationResponseDto> (this.createCompleteRoute(route, this.baseApiUrl), body);
+  }
+
+  public loginUser = (route: string, body: UserForAuthenticationDto) => {
+    return this.http.post<AuthResponseDto>(this.createCompleteRoute(route, this.baseApiUrl), body);
   }
 
   private createCompleteRoute = (route: string, envAddress: string) => {
